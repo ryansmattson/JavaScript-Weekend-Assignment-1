60489ee06a099ed2a08385af72d25346b1a03d7a
Ryan Mattson <ryansmattson@gmail.com>
2016-06-26 09:37:16 -0500

    Finish basic functionality.  The program runs and does the calculations.  HARD MODE is finished, but needs more work.  PRO MODE is not done yet.

--- app.js
+++ app.js
@@ -1,16 +1,30 @@
-var claim1 = new Claim("John Doe", "Specialist", 1100);
+var claim1 = new Claim('John Doe', 'Specialist', 1100);
 
-var claim2 = new Claim("Jane Doe", "Optical", 100);
+var claim2 = new Claim('Jane Doe', 'Optical', 100);
 
-var claim3 = new Claim("Joe Johnson", "Emergency", 31000);
+var claim3 = new Claim('Joe Johnson', 'Emergency', 31000);
 
-var claim4 = new Claim("Sharon Smith", "Emergency", 1300);
+var claim4 = new Claim('Sharon Smith', 'Emergency', 1300);
 
-var claim5 = new Claim("Steve Wright", "Primary Care", 770);
+var claim5 = new Claim('Steve Wright', 'Primary Care', 770);
 
-var initialList = [claim1, claim2, claim3, claim4, claim5]
+var claim6 = new Claim('Ryan Mattson', 'Optical', 150);
+
+var claim7 = new Claim('Ryan Mulcahy', 'Emergency', 13000);
+
+var claim8 = new Claim('Joel Miller', 'Specialist', 8000);
+
+var claim9 = new Claim('Bob Johnson', 'Specialist', 300);
+
+var claim10 = new Claim('John Deer', 'Primary Care', 500);
+
+var initialList = [claim1, claim2, claim3, claim4, claim5, claim6, claim7, claim8, claim9, claim10]
 
 var totalPayedOut = 0;
+
+//final array showing how much is paid out to each client
+//Paid out $' + amount + ' for ' + claim.patientName
+var paidOutPerClient = '';
 
 function Claim(name, type, cost){
 	this.patientName = name;
@@ -18,6 +32,53 @@ function Claim(name, type, cost){
 	this.visitCost = cost;
 }
 
-//function to determine percent covered
+console.log(initialList);
 
-//function to determine amount covered
+//function to determine percent covered
+function percentageCovered(claim){
+
+	var percent = 0;
+//calculate how what percentage of the claim is covered for each visitType.
+	switch(claim.visitType) {
+		case 'Optical':
+			percent = 0;
+			break;
+		case 'Specialist':
+			percent = 10;
+			break;
+		case 'Emergency':
+			percent = 100;
+			break;
+		case 'Primary Care':
+			percent = 50;
+			break;
+	}
+//return the percentage of the claim covered.
+	return percent;
+}
+
+//function to determine amount covered and log out for each patient.
+function amountCovered(claim){
+	var percent = percentageCovered(claim);
+//amount paid out rounded to the nearest dollar amount.
+	var amount = Math.round(claim.visitCost * (percent / 100));
+//add to the total money paid out.  Logged at the end.
+	totalPayedOut += amount;
+	paidOutPerClient += ('Paid out $' + amount + ' for ' + claim.patientName + ' \n');
+	$("p")
+	console.log('Paid out $' + amount + ' for ' + claim.patientName);
+}
+
+//iterate through the claims array.
+for (var i = 0; i < initialList.length; i++) {
+	amountCovered(initialList[i]);
+}
+
+//log out the total amount paid out.
+console.log('Total paid out: ', totalPayedOut);
+console.log(paidOutPerClient);
+
+//jQuery
+$(document).ready(function(){
+	$(".list").append(paidOutPerClient);
+})
